14aba9843f60e17d2b96c9e282409c00918f9c52
sandu456 <sadupamasri456@gmail.com>
2024-12-13 23:01:27 -0800

    Update CustomerService.java

--- mealsdelivery/mealsdelivery/src/main/java/com/example/mealsdelivery/Service/CustomerService.java
+++ mealsdelivery/mealsdelivery/src/main/java/com/example/mealsdelivery/Service/CustomerService.java
@@ -1,9 +1,38 @@
-package com.example.mealsdelivery.Service;
+package Service;
 
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import Repository.CustomerRepository;
+import models.Customer;
 
+@Service
 public class CustomerService {
 
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public Optional<Customer> getCustomerById(String id) {
+        return customerRepository.findById(id);
+    }
+
+    public Customer createCustomer(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    public Customer updateCustomer(String id, Customer customer) {
+        customer.setId(id);
+        return customerRepository.save(customer);
+    }
+
+    public void deleteCustomer(String id) {
+        customerRepository.deleteById(id);
+    }
 }
 
